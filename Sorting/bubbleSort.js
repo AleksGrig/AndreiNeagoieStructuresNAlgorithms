7570f13f69b29c2685539e4134fa88b3cb5c1e78
Aleks Grig <ullitap@gmail.com>
2019-07-27 11:05:22 +0300

    bubbleSort

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,15 @@
+function bubbleSort(array) {
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 0; j < array.length - i - 1; j++) {
+            if(array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+let array = [4, 10, 6, 7, 0, 3];
+console.log(bubbleSort(array));
