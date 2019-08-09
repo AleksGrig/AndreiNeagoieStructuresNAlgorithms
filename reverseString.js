8c2831deac01deefd9605c3435b5bd71dbb6ea86
Aleks Grig <ullitap@gmail.com>
2019-08-09 10:11:06 +0300

    recursion

--- reverseString.js
+++ reverseString.js
@@ -18,6 +18,12 @@ function reverse2(str){
 
 const reverse3 = str => [...str].reverse().join('');
 
+const reverse5 = str => str.split('').reverse().join('');
+
+function reverse4(str) {
+  return [...str].reverse().join('');
+}
+
 function reverseString(str) {
     let arrayStr = str.split("");
     let reversedArray = [];
@@ -28,15 +34,37 @@ function reverseString(str) {
 }
 
 function reverseStringRecursive (str) {
-    if (str === "") {
-      return "";
+  if (str === "") {
+    return "";
     } else {
       return reverseStringRecursive(str.substr(1)) + str.charAt(0);
-    }
   }
+}
 
-console.log(reverse1('alex'));
+function reverseNumber(num) {
+  let positive = num >= 0 ? true : false;
+  num = positive ? num : -1 * num; 
+  let res = 0;
+  let reversedNumber = 0;
+  while(num !== 0) {
+    res = num % 10;
+    num = Math.floor(num / 10);
+    reversedNumber = reversedNumber * 10 + res;
+  }
+  return positive ? reversedNumber : -1 * reversedNumber;
+}
+
+function reverseNumber2(num) {
+  let str = "";
+  str += num;
+  return str.split('').reverse().join('');
+}
+
+console.log(reverse5('alex'));
 console.log(reverse2('Hi there!'));
 console.log(reverse3('peace!'));
+console.log(reverse4('World!'));
 console.log(reverseString('peace!!!'));
 console.log(reverseStringRecursive('Aurelius'));
+console.log(reverseNumber(12345));
+console.log(reverseNumber2(567));
